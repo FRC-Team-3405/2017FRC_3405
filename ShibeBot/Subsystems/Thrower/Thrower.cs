9cbb500c192aef629284692a8a8a5c55f4808af5
SwagStormer <darthryan6265@gmail.comgit>
2017-03-09 10:30:55 -0700

    Added some thrower code

--- ShibeBot/Subsystems/Thrower/Thrower.cs
+++ ShibeBot/Subsystems/Thrower/Thrower.cs
@@ -1,6 +1,7 @@
 ﻿using WPILib;
 using WPILib.Commands;
 using WPILib.Interfaces;
+using System.Threading;
 
 namespace ShibeBot.Subsystems.Thrower
 {
@@ -8,6 +9,7 @@ namespace ShibeBot.Subsystems.Thrower
     {
         private static readonly ISpeedController Left = new Talon(RobotMap.ThrowerLeft);
         private static readonly ISpeedController Right = new Talon(RobotMap.ThrowerRight);
+		private static readonly ISpeedController Loader = new Talon(RobotMap.ThrowerLoader);
         private static readonly Encoder LeftEncoder = new Encoder(RobotMap.ThrowerLeftQuadratureA, RobotMap.ThrowerLeftQuadratureB);
         private static readonly Encoder RightEncoder = new Encoder(RobotMap.ThrowerRightQuadratureA, RobotMap.ThrowerRightQuadratureB);
         private static readonly AnalogPotentiometer Angle = new AnalogPotentiometer(RobotMap.ThrowerAngle);
@@ -17,7 +19,12 @@ namespace ShibeBot.Subsystems.Thrower
 		private double _left = 1;
 		private double _right = 1;
 
-		// Are we throwing?
+
+
+		public Thrower() 
+		{
+			Loader.Set(-.5);
+		}
 
 
         protected override void InitDefaultCommand()
@@ -25,23 +32,30 @@ namespace ShibeBot.Subsystems.Thrower
             
         }
 
-		public void Throw(Joystick controller)
+		//public void Throw(Joystick controller)
+		//{
+		//	double leftRate = LeftEncoder.GetRate();
+		//	double rightRate = RightEncoder.GetRate();
+
+		//	if (leftRate > rightRate)
+		//	{
+		//		// Decrement left side
+		//	}
+		//	else if (leftRate < rightRate) 
+		//	{
+		//		// Decrement right side
+		//	}
+
+		//	Right.Set(_right);
+		//	Left.Set(_left);
+		//}
+
+		public void Throw(Joystick controller) 
 		{
-			double leftRate = LeftEncoder.GetRate();
-			double rightRate = RightEncoder.GetRate();
-
-			if (leftRate > rightRate)
-			{
-				// Decrement left side
-			}
-			else if (leftRate < rightRate) 
-			{
-				// Decrement right side
-			}
-
-			Right.Set(_right);
-			Left.Set(_left);
+			Left.Set(1);
+			Right.Set(1);
+			Thread.Sleep(200);
+			Loader.Set(1);
 		}
-
     }
 }
