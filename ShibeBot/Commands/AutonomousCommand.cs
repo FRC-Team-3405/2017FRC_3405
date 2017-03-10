21757a56d2c4ce6a54b15e22d40921206b597393
SwagStormer <darthryan6265@gmail.comgit>
2017-03-10 08:51:33 -0700

    Autonomous running

--- ShibeBot/Commands/AutonomousCommand.cs
+++ ShibeBot/Commands/AutonomousCommand.cs
@@ -27,7 +27,7 @@ namespace ShibeBot.Commands
 		// Make this return true when this Command no longer needs to run execute()
 		protected override bool IsFinished()
 		{
-			return false;
+			return true;
 		}
 
 		// Called once after isFinished returns true
