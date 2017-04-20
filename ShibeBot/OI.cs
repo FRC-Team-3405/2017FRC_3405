091b54e8e7ef91d286bae5cf1a2cf4de8041a7d8
SwagStormer <ryan@ryansbees.com>
2017-04-20 11:28:19 -0600

    default commande

--- ShibeBot/OI.cs
+++ ShibeBot/OI.cs
@@ -5,47 +5,14 @@ using ShibeBot.OperatorInteface;
 
 namespace ShibeBot
 {
-
-    public enum DriveStyle
-    {
-        Arcade,
-        Tank
-    }
-
-
-    public enum ToggleField
-    {
-        DriveStyle
-    }
-
-    public class Oi
-    {
-        public static bool JoysticksSwitched = false;
-
+	public class Oi
+	{
 		public static Joystick Pilot = new Joystick(HidMap.PilotXbox);
-        public static Joystick Copilot = new Joystick(HidMap.CoPilotXbox);
 
-        //Pilot Systems
-        public static JoystickButton ArcadeToggle = new JoystickButton(Pilot, XboxMap.AButton);
-        public static JoystickButton TankToggle = new JoystickButton(Pilot, XboxMap.XButton);
-		public static JoystickButton HigherGear = new JoystickButton(Pilot, XboxMap.RightBumper);
-        public static JoystickButton LowerGear = new JoystickButton(Pilot, XboxMap.LeftBumper);
-        public static JoystickStick LeftStick = new JoystickStick(Pilot, XboxMap.LeftX, XboxMap.LeftY);
-        public static JoystickStick RightStick = new JoystickStick(Pilot, XboxMap.RightX, XboxMap.RightY);
+		public Oi()
+		{
 
-        //Pilot Variables
-        public static DriveStyle DriveStyle = DriveStyle.Arcade;
-
-
-        //Co-Pilot Variables
-	    public static bool CollectorEnabled = false;
-	    public static bool ThrowerEnabled = false;
-	    public static bool HooperStirEnabled = false;
-	    public static bool HopperFeedEnabled = false;
-
-        public Oi() {
-            
-        }
+		}
 
 	}
 }
--- ShibeBot/Subsystems/DriveTrain/Drive.cs
+++ ShibeBot/Subsystems/DriveTrain/Drive.cs
@@ -17,38 +17,17 @@ namespace ShibeBot.Subsystems.DriveTrain
 		private static readonly Talon RightSecondary = new Talon(3);
 
 		public RobotDrive _drive = new RobotDrive(LeftPrimary, LeftSecondary, RightPrimary, LeftSecondary);
-		private int _driveSpeed;
 
-        protected override void InitDefaultCommand()
-        {
-           SetDefaultCommand(new DriveCommand());
-        }
+		protected override void InitDefaultCommand()
+		{
+			SetDefaultCommand(new DriveCommand());
+		}
 
-        private double leftX = 0;
-        private double leftY = 0;
-        private double rightX = 0;
-        private double rightY = 0;
 
 		public void Drive(Joystick stick)
 		{
 			_drive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), 1);
 		}
 
-        private void AutoLerp(Joystick stick, double amount)
-        {
-            leftX = Lerp(leftX, stick.GetRawAxis(XboxMap.LeftX), amount);
-            leftY = Lerp(leftY, stick.GetRawAxis(XboxMap.LeftY), amount);
-            rightX = Lerp(rightX, stick.GetRawAxis(XboxMap.RightX), amount);
-            rightY = Lerp(rightY, stick.GetRawAxis(XboxMap.RightY), amount);
-            SmartDashboard.PutNumber("Left X", leftX);
-            SmartDashboard.PutNumber("Left Y", leftY);
-            SmartDashboard.PutNumber("Right X", rightX);
-            SmartDashboard.PutNumber("Right Y", rightY);
-        }
-
-        private double Lerp(double first, double second, double amount)
-        {
-            return (1 - amount) * first + amount * second;
-        }
     }
 }
